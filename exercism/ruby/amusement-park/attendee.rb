496d7ccec2e775ee8434071b9faff63fe726d8ea
HungryNomad <42802559+HungryNomad@users.noreply.github.com>
2023-11-09 09:45:19 +0800

    solved

--- exercism/ruby/amusement-park/attendee.rb
+++ exercism/ruby/amusement-park/attendee.rb
@@ -1,21 +1,15 @@
 class Attendee
   def initialize(height)
-    raise 'Implement the Attendee#initialize method'
+    @height = height
   end
 
-  def height
-    raise 'Implement the Attendee#height method'
-  end
-
-  def pass_id
-    raise 'Implement the Attendee#pass_id method'
-  end
+  attr_reader :height, :pass_id
 
   def issue_pass!(pass_id)
-    raise 'Implement the Attendee#issue_pass! method'
+    @pass_id = pass_id
   end
 
   def revoke_pass!
-    raise 'Implement the Attendee#revoke_pass! method'
+    @pass_id = nil
   end
 end
