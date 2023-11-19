ed7faa47b81f14b0d7c47556bbccffe4a2ea341a
HungryNomad <42802559+HungryNomad@users.noreply.github.com>
2023-11-19 16:49:48 -0500

    refactored

--- exercism/ruby/simple-calculator/simple_calculator.rb
+++ exercism/ruby/simple-calculator/simple_calculator.rb
@@ -5,8 +5,10 @@ class SimpleCalculator
   def self.calculate(first_operand, second_operand, operation)
     raise ArgumentError unless (first_operand.is_a? Numeric) && (second_operand.is_a? Numeric)
     raise UnsupportedOperation unless ALLOWED_OPERATIONS.include?(operation)
-    return 'Division by zero is not allowed.' if operation == '/' && second_operand.zero?
+    raise ZeroDivisionError, 'Division by zero is not allowed.' if operation == '/' && second_operand.zero?
 
     "#{first_operand} #{operation} #{second_operand} = #{first_operand.send(operation, second_operand)}"
+  rescue ZeroDivisionError => e
+    e.message
   end
 end
